f2ca16f18387c42b87d34d104278784553819c48
averym16 <averymorrison26@gmail.com>
2025-10-23 16:30:39 -0400

    Add jquery to dynamically create footer for all pages

--- scripts/commen_elements.js
+++ scripts/commen_elements.js
@@ -1,8 +1,5 @@
-
-import $ from jQuery
-
 $(document).ready(function() {
     const now = new Date();
-    $("#footer-info").html('<div id="contact_info"><h3>Contact Us</h3><p>Email: <a href="https://www.youtube.com/watch?v=xeSpzHqQT2w&t=0h0m46s" target="_blank">averymorrison26@gmail.com</a></p><p>Github:<a href="https://github.com/averym16" target="_blank">averym16</a></p></div>');
-    $("#footer-info").append('<div id="footer-misc"><p><a href="../CREDITS.txt">Sources</a></p><p>Last Updated: >10/21/2025 18:20:00</p><p>Licensed by <a href="../LICENSE.txt">MIT</a></p>')
+    $("#footer-info").html('<section id="contact_info"><h3>Contact Us</h3><p>Email: <a href="https://www.youtube.com/watch?v=xeSpzHqQT2w&t=0h0m46s" target="_blank">averymorrison26@gmail.com</a></p><p>Github:<a href="https://github.com/averym16" target="_blank">averym16</a></p></section>');
+    $("#footer-info").append('<section id="footer-misc"><p><a href="../CREDITS.txt">Sources</a></p><p>Last Updated: 10/21/2025 18:20:00</p><p>Licensed by <a href="../LICENSE.txt">MIT</a></p></section>');
 });
